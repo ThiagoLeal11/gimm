f74f96f3e3e78e1884c39379c85b0ad62cfec51a
Thiago L. Pozati <thiagoleal11@gmail.com>
2025-10-30 22:19:24 -0300

    Feat: Add toucan databases and removed device

--- gimm/run/loader.py
+++ gimm/run/loader.py
@@ -1,14 +1,15 @@
-import torch
-
 from gimm.datasets.definition import Dataset
 
 
-def dataset_loader(name: str, batch_size: int, num_workers: int, device: torch.device, data_dir: str) -> Dataset:
+def dataset_loader(name: str, batch_size: int, num_workers: int, data_dir: str) -> Dataset:
     if name == "cifar10":
         from gimm.datasets.cifar10 import DatasetCifar10
-        return DatasetCifar10(batch_size=batch_size, num_workers=num_workers, pin_memory_device=device, data_dir=data_dir)
+        return DatasetCifar10(batch_size=batch_size, num_workers=num_workers, data_dir=data_dir)
     elif name == "mnist":
         from gimm.datasets.mnist import DatasetMNIST
-        return DatasetMNIST(batch_size=batch_size, num_workers=num_workers, pin_memory_device=device, data_dir=data_dir)
+        return DatasetMNIST(batch_size=batch_size, num_workers=num_workers, data_dir=data_dir)
+    elif name == "toucan":
+        from gimm.datasets.toucan import DatasetToucan
+        return DatasetToucan(batch_size=batch_size, num_workers=num_workers, data_dir=data_dir)
     else:
         raise ValueError(f"Dataset {name} not found.")
